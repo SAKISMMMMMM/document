afdb8ba5ab5818ad8f7d573487b20828856a186b
何允宇 <yunyu.he@kingold.com>
2021-06-30 18:00:00 +0800

    doc: change doc

--- 原理or源码/JavaScript/Promise/index.js
+++ 原理or源码/JavaScript/Promise/index.js
@@ -1,13 +1,39 @@
+console.log('script start')
 
-new Promise(getB).then(
-  (res) => {
-    console.log('a,start');
-    return new Promise(r => setTimeout(() => r(3), 3000))
-  }).then(() => console.log('end'))
+new Promise(r=>{
+  console.time('request Time')
+  axios.get('https://movie.querydata.org/api').then(res=>r(res))
+  console.log('request')
+  // r(requestResult)
+}).then(res=>{
+  console.timeEnd('request Time')
+  console.log('request result', res);
+}).then(res=>console.log('1.1')).then(res=>console.log('1.2')).then(res=>console.log('1.3'))
+
+setTimeout(()=>{
+  console.log('timer0s')
+},0)
+
+setTimeout(()=>{
+  console.log('timer1s')
+},1000)
+
+setTimeout(()=>{
+  console.log('timer2s')
+},2000)
+
+setTimeout(()=>{
+  console.log('timer3s')
+},3000)
 
 
-new Promise(getB).then(
-  (res) => {
-    console.log('a,start');
-    return new Promise(r => setTimeout(() => r(3), 3000))
-  }).then(() => console.log('end'))
+
+
+new Promise(r=>{
+  console.log('local promise')
+  r()
+}).then(()=>{
+  console.log('local promise then')
+})
+
+console.log('script end')
